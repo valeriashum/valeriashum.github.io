46f772b4f531c15c4995a2c38b304fc5af9f3d4a
jys26 <judy.shing28@gmail.com>
2016-03-09 18:43:32 +0000

    new

--- myapp_copy.py
+++ myapp_copy.py
@@ -12,6 +12,46 @@ email_addresses=[]
 def hello_world(): 
     return render_template('index.html')
 
+@app.route("/index.html")
+def hello_world1(): 
+    return render_template('index.html')
+    
+@app.route("/climate_change.html")
+def hello_world2(): 
+    return render_template('climate_change.html')
+    
+@app.route("/podcasts.html")
+def hello_world3(): 
+    return render_template('podcasts.html')
+    
+@app.route("/refugee_crisis.html")
+def hello_world4(): 
+    return render_template('refugee_crisis.html')
+    
+@app.route("/blog-post.html")
+def hello_world5(): 
+    return render_template('blog-post.html')
+    
+@app.route("/blog-post1.html")
+def hello_world6(): 
+    return render_template('blog-post1.html')
+    
+@app.route("/blog-post2.html")
+def hello_world7(): 
+    return render_template('blog-post2.html')
+
+@app.route("/blog.html")
+def hello_world10(): 
+    return render_template('blog.html')
+
+@app.route("/portfolio.html")
+def hello_world8(): 
+    return render_template('portfolio.html')
+    
+@app.route("/videos.html")
+def hello_world9(): 
+    return render_template('videos.html')
+    
 @app.route("/signup", methods = ['POST'])
 def signup():
     email = request.form['email']
@@ -19,16 +59,9 @@ def signup():
     print(email_addresses)
     return redirect('/thankyou')
 
-@app.route('/thankyou.html')
-	requests.post(
-		"https://api.mailgun.net/v3/sandbox16522a5cfadb400b83c6c3c1db813b96.mailgun.org/messages",
-        auth=("api", "key-d3df711eb1e752187c7cda1e2520fa78"),
-        data={"from": "Judy <judy.shing28@gmail.com>",
-              "to": "Customer "<" + email + ">",
-               "subject": "Hello for Hall Talk",
-               "text": "Congratulations, you just signed up to Hall Talk !"
-             }
-	)
+@app.route('/thankyou')
+def thankyou():
+	
 	return render_template('thankyou.html')
 
 @app.route('/emails')
